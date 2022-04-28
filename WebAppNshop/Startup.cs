f6113d8d33c651559bf87afd56898d92b1225a19
Tinh <nguyentinh14032001@gmail.com>
2022-04-28 14:18:38 +0700

    Lịch sử giao dịch, QL sản phẩm, thông tin tài khoản, nguyên tính

--- WebAppNshop/Startup.cs
+++ WebAppNshop/Startup.cs
@@ -57,7 +57,7 @@ namespace WebAppNshop
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Account}/{action=Index}/{id?}");
             });
         }
     }
