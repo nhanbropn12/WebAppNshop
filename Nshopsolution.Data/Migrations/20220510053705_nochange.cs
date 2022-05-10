6142d435b7adde542cabb9d290f8ddf9526c4d4a
nhanbropn12 <nhanbropn12@gmail.com>
2022-05-10 12:40:16 +0700

    Update net core 2.2 to net core 3.1, and add setting.json in WebAppNshop project

---
+++ Nshopsolution.Data/Migrations/20220510053705_nochange.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nshopsolution.Data.Migrations
+{
+    public partial class nochange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreate", "DateOfBirth" },
+                values: new object[] { new DateTime(2022, 5, 10, 12, 37, 5, 495, DateTimeKind.Local).AddTicks(9300), new DateTime(2022, 5, 10, 12, 37, 5, 495, DateTimeKind.Local).AddTicks(1140) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreate", "DateOfBirth" },
+                values: new object[] { new DateTime(2022, 5, 10, 9, 33, 42, 457, DateTimeKind.Local).AddTicks(5346), new DateTime(2022, 5, 10, 9, 33, 42, 456, DateTimeKind.Local).AddTicks(2794) });
+        }
+    }
+}
